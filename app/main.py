7274ef65843508aadac3fe9b8ef566a4dac99a89
Abhay Singh <121842845+Icarus-si@users.noreply.github.com>
2026-09-18 20:06:49 +1000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,13 +15,18 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# Prometheus monitoring
+Instrumentator().instrument(app).expose(app, endpoint="/metrics")
+
 UPLOAD_DIR = "data/cases"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 rag_chain = None
 
+
 class QuestionInput(BaseModel):
     question: str
+
 
 @app.get("/")
 def root():
@@ -32,34 +37,46 @@ def root():
         "author": "Abhay Singh Wazir"
     }
 
+
 @app.post("/ingest")
 async def ingest_pdfs(files: List[UploadFile] = File(...)):
     global rag_chain
     saved_paths = []
+
     for file in files:
         if not file.filename.endswith(".pdf"):
-            raise HTTPException(status_code=400, detail=f"{file.filename} is not a PDF")
+            raise HTTPException(
+                status_code=400,
+                detail=f"{file.filename} is not a PDF"
+            )
+
         path = os.path.join(UPLOAD_DIR, file.filename)
+
         with open(path, "wb") as f:
             content = await file.read()
             f.write(content)
+
         saved_paths.append(path)
 
     try:
         num_chunks = load_and_index_pdfs(saved_paths)
         vectorstore = load_vectorstore()
         rag_chain = get_rag_chain(vectorstore)
+
         return {
             "message": f"Successfully ingested {len(saved_paths)} PDF(s)",
             "chunks_created": num_chunks,
             "files": [file.filename for file in files]
         }
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.post("/ask")
 def ask(input: QuestionInput):
     global rag_chain
+
     if rag_chain is None:
         try:
             vectorstore = load_vectorstore()
@@ -69,11 +86,14 @@ def ask(input: QuestionInput):
                 status_code=400,
                 detail="No documents indexed yet. Please upload PDFs first via /ingest"
             )
+
     try:
         result = ask_question(rag_chain, input.question)
         return result
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.get("/health")
 def health():
